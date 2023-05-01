d9e68016cc0877457e3e4170b6c33d274d3bb5a9
KangAbbad <naufalabbad9@gmail.com>
2023-05-01 09:48:41 +0700

    feat: dark mode setup

---
+++ src/hooks/useThemeSwithcer.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+const useThemeSwithcer = () => {
+  const [mode, setMode] = useState("");
+  const preferDarkQuery = "(prefer-color-scheme: dark)";
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(preferDarkQuery);
+    const userPref = localStorage.getItem("theme");
+
+    const handleChange = () => {
+      if (userPref) {
+        let check = userPref === "dark" ? "dark" : "light";
+        setMode(check);
+        if (check === "dark") {
+          document.documentElement.classList.add("dark");
+        } else {
+          document.documentElement.classList.remove("dark");
+        }
+      } else {
+        let check = mediaQuery.matches ? "dark" : "light";
+        setMode(check);
+        if (check === "dark") {
+          document.documentElement.classList.add("dark");
+        } else {
+          document.documentElement.classList.remove("dark");
+        }
+      }
+      mediaQuery.addEventListener("change", handleChange);
+      return () => mediaQuery.removeEventListener("change", handleChange);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (mode === "dark") {
+      localStorage.setItem("theme", "dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      localStorage.setItem("theme", "light");
+      document.documentElement.classList.remove("dark");
+    }
+  }, [mode]);
+
+  return [mode, setMode];
+};
+
+export default useThemeSwithcer;
